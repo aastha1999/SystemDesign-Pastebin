c4f62247b6a16a8f3cc00ee0c3a32c3fb319a604
Ashutosh Sahu <ashutoshsahu@iitbhilai.ac.in>
2022-02-12 02:03:05 +0530

    Fix allowed hosts

--- pasteRand/settings.py
+++ pasteRand/settings.py
@@ -35,7 +35,7 @@ AWS_PASSWORD = os.environ['AWS_PASSWORD']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
