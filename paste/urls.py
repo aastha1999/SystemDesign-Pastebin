7077dc8d3df859917ba299712db0164606a5a8c1
Priyankay151 <55487715+Priyankay151@users.noreply.github.com>
2022-02-13 15:55:48 +0530

    Update urls.py

--- paste/urls.py
+++ paste/urls.py
@@ -17,10 +17,8 @@ urlpatterns = [
 
 
     ## API calls
-    path('api/get_content/', api_detail_paste_view, name='get_content'),
+
     path('api/account/register/', registration_view, name='register'),
-    path('api/account/login/', registration_view, name='login'),
-    # path('api/get_content/',include('paste.api.urls','paste_api')),
 
 
     # get slug details
@@ -31,45 +29,45 @@ urlpatterns = [
     # get raw content of slug
     # input: slug
     # output: PasteFile contents
-    path('raw/<str:slug>/', RawContent.as_view(), name='raw_content'),
+    path('api/raw/<str:slug>/', RawContent.as_view(), name='raw_content'),
 
 
     # create URL
     # input: content and title
     # output: slug
     # logic: if title & content already exists, return existing slug, else return new slug
-    path('p/createURL/', GetUrl.as_view(), name='create_url'),
+    path('api/createURL/', GetUrl.as_view(), name='create_url'),
 
 
     # create URL
     # input: title, content
     # output: slug
-    path('p/create/', CreateNewPaste.as_view(), name='create'),
+    path('api/create/', CreateNewPaste.as_view(), name='create'),
 
 
     # delete an existing slug
     # input: slug
-    path('p/delete/', deleteUrl.as_view(), name='delete'),
+    path('api/delete/', deleteUrl.as_view(), name='delete'),
 
 
     # get contents of an existing slug
     # input: slug
     # output: PasteFile contents
-    path('p/get_content/', GetRawPaste.as_view(), name='get_content'),
+    path('api/get_content/', GetRawPaste.as_view(), name='get_content'),
 
 
     # get token of an existing user
     # input: 
     # output: token ID
-    path('p/getToken/', GetToken.as_view(), name='get_user'),
+    path('api/getToken/', GetToken.as_view(), name='get_user'),
 
     
     # get user details
     # input: 
     # output: 
-    path('p/getUser/', GetUser.as_view(), name='get_user'),
+    path('api/getUser/', GetUser.as_view(), name='get_user'),
 
 
     # get all pastes from the database
-    path('p/allpastes/', AllPastes.as_view(), name='all_pastes')
+    path('api/allpastes/', AllPastes.as_view(), name='all_pastes')
 ]
